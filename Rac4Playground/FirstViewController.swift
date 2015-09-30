ff9f12b904e4c997323558bcbd5300f699863aa9
Edward Ishaq <edward.ishaq@gmail.com>
2015-09-30 16:12:45 -0700

    initial Commit

--- Rac4Playground/FirstViewController.swift
+++ Rac4Playground/FirstViewController.swift
@@ -7,14 +7,78 @@
 //
 
 import UIKit
+import ReactiveCocoa
+
+func makeArray(text: String) -> [String] {
+    var result = [String]()
+    for _ in 0...5 {
+        result.append(text)
+    }
+    return result
+}
+
+
+class DummyViewModel {
+    let searchText = MutableProperty<String>("")
+    
+    func arraySignal(text: String) -> SignalProducer<String, NoError> {
+        let arrayProducer = SignalProducer<String, NoError>(values: makeArray(text))
+        return arrayProducer
+    }
+    
+}
 
 class FirstViewController: UIViewController {
-
+    @IBOutlet weak var searchField: UITextField!
+    @IBOutlet weak var button: UIButton!
+    let viewModel = DummyViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        bindViewModel()
     }
 
+    func bindViewModel() {
+        let searchStrings = searchField.rac_textSignal()
+            .toSignalProducer()
+            .map{ text in text as! String}
+        
+
+        let searchStringsNoError = searchStrings.flatMapError { (error:NSError) -> SignalProducer<String, NoError> in
+            return SignalProducer<String, NoError>.empty
+        }
+        
+        let flatMapped = searchStringsNoError.flatMap(.Latest) { (txt: String) in
+            return self.viewModel.arraySignal(txt)
+        }
+        
+        flatMapped.on(next: { txt in
+            print("flatMapped \(txt)")
+        })
+        .start()
+//        searchStringsNoError.flatMap(.Latest) { txt in
+//            return self.viewModel.arraySignal(txt).promoteError(NSError.self)
+//        }
+
+
+//        searchStrings.flatMap(.Latest) { txt in
+//            self.viewModel.arraySignal(txt).promoteError(NSError.self)
+//        }
+        
+//        searchStrings
+//            .mapError{ err ->  SignalProducer<String, NoError> in
+//                SignalProducer<String, NoError>.empty
+//            }
+        
+//        searchStrings.producer
+//            .promoteError(NoError.self)
+//            .flatMap(.Latest) { text in
+//            return viewModel.arraySignal(text)
+//        }
+
+        
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
